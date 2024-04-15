af34767fa03c60534de15c51fc25b4d54ecd5b56
Bedis Nbiba <bedisnbiba@gmail.com>
2024-04-15 20:13:49 +0100

    Update sw.js

--- assets/sw.js
+++ assets/sw.js
@@ -5,16 +5,19 @@ const cacheName = "myapp";
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     (async () => {
-      const response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-      // if we have no network access, try to use the cache
-      const r = await caches.match(e.request);
-      console.log(`[Service Worker] Using cached resource: ${e.request.url}`);
-      if (r) {
-        return r;
+      try {
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        console.log(`[Service Worker] Caching resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+      } catch {
+        // if we have no network access, try to use the cache
+        const response = await caches.match(e.request);
+        console.log(`[Service Worker] Using cached resource: ${e.request.url}`);
+        if (response) {
+          return response;
+        }
       }
     })(),
   );
